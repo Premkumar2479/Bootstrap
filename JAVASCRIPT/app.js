ef051d21792201ab3fa22a5d2bd303de675a616d
Premkumar2479 <pk2335301@gmail.com>
2025-12-24 10:38:18 +0530

    add file

---
+++ JAVASCRIPT/app.js
@@ -0,0 +1,44 @@
+// alert("Welcome to javascript programming!");
+//document.write("<h1>Welcome to javascript</h1>");
+// document.write("<h1 style='color: blue;'>Welcome to javascript</h1>");
+// console.log("welcome to javascript programming!");
+// prompt("Enter your name:");
+// confirm("Are you sure you want to proceed?");
+
+//write a program to add two numbers
+// var name = Prompt("Enter your name:");
+// document.write("your name is " + name);
+
+//take two variable first name and last name and join them.
+
+// var fristName = "Nick";
+// var lastName ="jonas";
+// document.write(fristName + " " + lastName);
+
+
+//write a program to find the sum of two numbers
+//write a program to find the area of square
+//wirte a program to find the area and perimeter of  a rectangle
+
+var num1=10;
+var num2=20;
+var sum = num1 + num2;
+document.write(sum);
+
+
+//write a program to find the area of square
+
+var side =5;
+var area = side *side;
+document.write("Area of square is : " + area);
+
+//wirte a program to find the area and perimeter of  a rectangle
+
+var length = 10;
+var breadth = 5;
+var areaRect =  Length *breadth;
+var perimeter = 2 * (length + breadth);
+document.write("Area of rectangle is : " + areaRect);
+document.write("Perimeter of rectangle is : " + perimeter);
+
+
